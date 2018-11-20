41d72bf080405bb07e49ea17a2e69027173cb4b4
wtranmer <wtranmer@gmail.com>
2018-11-20 03:23:59 -0500

    update build_libs.bat to work for multiple targets

--- build_libs.bat
+++ build_libs.bat
@@ -1,13 +1,14 @@
 set thispath=%~dp0
 set orcabinpath=d:\appeon\shared\powerbuilder\orcascr170.exe
+for %%i in (*.pbt) do (
 echo start session > build_libs.orca
 echo scc set connect property localprojpath "." >> build_libs.orca
 echo scc connect offline >> build_libs.orca
-for %%i in (*.pbt) do (
 echo scc set target ".\%%i" refresh_all importonly >> build_libs.orca
-)
 echo scc refresh target full >> build_libs.orca
 echo scc close >> build_libs.orca
 echo end session >> build_libs.orca
-
 "%orcabinpath%" build_libs.orca
+)
+
+pause
