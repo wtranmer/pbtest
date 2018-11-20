e7b0cf8b44cc4f4b7fb31473269b48b28ac180a0
wtranmer <wtranmer@gmail.com>
2018-11-20 02:49:45 -0500

    simplify build_libs.bat

--- build_libs.bat
+++ build_libs.bat
@@ -1,10 +1,10 @@
 set thispath=%~dp0
 set orcabinpath=d:\appeon\shared\powerbuilder\orcascr170.exe
 echo start session > build_libs.orca
-echo scc set connect property localprojpath "%thispath%" >> build_libs.orca
+echo scc set connect property localprojpath "." >> build_libs.orca
 echo scc connect offline >> build_libs.orca
 for %%i in (*.pbt) do (
-echo scc set target "%thispath%%%i" refresh_all importonly >> build_libs.orca
+echo scc set target ".\%%i" refresh_all importonly >> build_libs.orca
 )
 echo scc refresh target full >> build_libs.orca
 echo scc close >> build_libs.orca
